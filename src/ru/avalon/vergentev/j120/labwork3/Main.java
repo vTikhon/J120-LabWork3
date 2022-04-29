95731741f882470f9ec38a95441a64ebac864a26
vTikhon <tikhon.v@gmail.com>
2022-04-29 16:52:33 +0300

    separate task1

--- src/ru/avalon/vergentev/j120/labwork3/Main.java
+++ src/ru/avalon/vergentev/j120/labwork3/Main.java
@@ -2,10 +2,6 @@ package ru.avalon.vergentev.j120.labwork3;
 
 public class Main {
     public static void main(String[] args) {
-        //>>>>>>>>>>>>>>>>>TASK 1<<<<<<<<<<<<<<<<<<<<<
         new Calculator().setVisible(true);
-
-        //>>>>>>>>>>>>>>>>>>TASK 2<<<<<<<<<<<<<<<<<<<<<
-        new Game().setVisible(true);
     }
 }
