44a80dc6ea807dee33ee2986b26226b93ca575ad
vTikhon <tikhon.v@gmail.com>
2022-04-29 16:46:44 +0300

    home set

--- src/ru/avalon/vergentev/j120/labwork3/Main.java
+++ src/ru/avalon/vergentev/j120/labwork3/Main.java
@@ -3,11 +3,9 @@ package ru.avalon.vergentev.j120.labwork3;
 public class Main {
     public static void main(String[] args) {
         //>>>>>>>>>>>>>>>>>TASK 1<<<<<<<<<<<<<<<<<<<<<
-//        Calculator myCalculator = new Calculator();
-//        myCalculator.setVisible(true);
+        new Calculator().setVisible(true);
 
         //>>>>>>>>>>>>>>>>>>TASK 2<<<<<<<<<<<<<<<<<<<<<
-        Game myGame = new Game();
-        myGame.setVisible(true);
+        new Game().setVisible(true);
     }
 }
